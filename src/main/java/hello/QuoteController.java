b6e0306b485cf8b3d64f539a366451e300e47b15
Marco Mangan <marco.mangan@gmail.com>
2019-06-26 14:55:19 -0300

    Adding Thymeleaf and REST integration.

--- src/main/java/hello/QuoteController.java
+++ src/main/java/hello/QuoteController.java
@@ -1,14 +1,23 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class QuoteController {
 
+	private static final Logger log = LoggerFactory.getLogger(QuoteController.class);
+	
 	@GetMapping("/quotes")
-	public String showQuote() {
-		
+	public String showQuote(RestTemplate restTemplate, Model model) {
+		Quote quote = restTemplate.getForObject(
+				"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+		log.info(quote.toString());		
+		model.addAttribute("q", quote);
 		return "quoteDetails";
 	}
 	
